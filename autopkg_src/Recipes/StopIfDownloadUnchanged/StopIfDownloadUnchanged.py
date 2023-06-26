db0e649d490da268ae00aee835bb1b0eff9e0578
NoahRJ <noah.anderson@gmail.com>
2023-06-25 19:32:43 -0500

    Trying daemon again

--- autopkg_src/Recipes/StopIfDownloadUnchanged/StopIfDownloadUnchanged.py
+++ autopkg_src/Recipes/StopIfDownloadUnchanged/StopIfDownloadUnchanged.py
@@ -53,8 +53,9 @@ class StopIfDownloadUnchanged(Processor):
         print("Stop Download proc now running")
         self.output("Stop Download proc now running")
         self.download_changed = None
-        bg_proc = multiprocessing.get_context("fork").Process(target=self.get_download_changed)
-        # bg_proc.daemon = True
+        # bg_proc = multiprocessing.get_context("fork").Process(target=self.get_download_changed)
+        bg_proc = multiprocessing.Process(target=self.get_download_changed)
+        bg_proc.daemon = True
         bg_proc.start()
         #exit(0)
 
